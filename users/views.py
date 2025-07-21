f1c595e10485e37ed1c714f4fc28cca25a7712ef
Shabna K S <shabna.ks@innovaturelabs.com>
2025-07-21 18:59:27 +0530

    fix:fixed the missing files

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import CustomUser
-from .serializers import RegisterSerializer
+#from .serializers import RegisterSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
